ed1845f8a1736c3878eb96a0a487dd9799252f96
Ugo Raffaele Piemontese <me@ugopiemontese.eu>
2016-03-01 14:47:45 +0100

    Setup.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,8 @@
 #include <pebble.h>
 #include "main.h"
+#include "storage.h"
 
+int main(void) {
+    app_event_loop();
+    return 0;
+}
--- worker_src/worker.c
+++ worker_src/worker.c
@@ -1,3 +1,8 @@
 #include <pebble_worker.h>
 #include "worker.h"
+#include "../src/storage.h"
 
+int main(void) {
+    worker_event_loop();
+    return 0;
+}
