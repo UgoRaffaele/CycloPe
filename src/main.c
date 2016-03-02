748ef7bf4b8da198397e0a0047c1af5dcafb38a5
Ugo Raffaele Piemontese <me@ugopiemontese.eu>
2016-03-02 01:48:33 +0100

    Refactoring with Data Logging APIs.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,94 @@
 #include <pebble.h>
 #include "main.h"
-#include "storage.h"
+
+static Window *s_main_window;
+static TextLayer *s_output_layer, *s_status_layer;
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+    
+    // Toggle running state
+    AppWorkerResult result;
+    
+    // Check to see if the worker is currently active
+    if (app_worker_is_running()) {
+        
+        result = app_worker_kill();
+
+        if (result == APP_WORKER_RESULT_SUCCESS) {
+            text_layer_set_text(s_status_layer, "Worker stopped!");
+        } else {
+            text_layer_set_text(s_status_layer, "Error killing worker!");
+        }
+        
+    } else {
+        
+        result = app_worker_launch();
+
+        if (result == APP_WORKER_RESULT_SUCCESS) {
+            text_layer_set_text(s_status_layer, "Worker launched!");
+        } else {
+            text_layer_set_text(s_status_layer, "Error launching worker!");
+        }
+        
+    }
+
+    APP_LOG(APP_LOG_LEVEL_INFO, "worker result: %d", result);
+    
+}
+
+static void main_window_load(Window *window) {
+    
+    // Create Window's child Layers here
+    Layer *window_layer = window_get_root_layer(window);
+    GRect window_bounds = layer_get_bounds(window_layer);
+
+    s_output_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
+    text_layer_set_text(s_output_layer, "Use SELECT to start/stop the background worker.");
+    text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+
+    s_status_layer = text_layer_create(GRect(0, 135, window_bounds.size.w, 30));
+    if (app_worker_is_running()) {
+        text_layer_set_text(s_status_layer, "Worker running!");
+    } else {
+        text_layer_set_text(s_status_layer, "Worker halted!");
+    }
+    text_layer_set_text_alignment(s_status_layer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(s_status_layer));
+    
+}
+
+static void main_window_unload(Window *window) {
+    
+    // Destroy Window's child Layers here
+    text_layer_destroy(s_output_layer);
+    text_layer_destroy(s_status_layer);
+    
+}
+
+static void click_config_provider(void *context) {
+    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+}
+
+static void init() {
+    // Create main Window
+    s_main_window = window_create();
+    window_set_click_config_provider(s_main_window, click_config_provider);
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+        .load = main_window_load,
+        .unload = main_window_unload,
+    });
+    window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+    // Destroy main Window
+    window_destroy(s_main_window);
+}
 
 int main(void) {
+    init();
     app_event_loop();
+    deinit();
     return 0;
 }
--- worker_src/worker.c
+++ worker_src/worker.c
@@ -1,73 +1,50 @@
 #include <pebble_worker.h>
-#include "../src/storage.h"
 
-static struct event current_page[PAGE_LENGTH];
-static unsigned index;
-static uint32_t num_samples = 2;
+#define ACCEL_BATCH 1
 
-static void append_event(struct event *event) {
-    
-	if (index >= PAGE_LENGTH) {
-		APP_LOG(APP_LOG_LEVEL_ERROR, "invalid value %u for index", index);
-		return;
-	}
-
-	current_page[index] = *event;
-	index = (index + 1) % PAGE_LENGTH;
-
-	int ret = persist_write_data(1, current_page, sizeof current_page);
-	if (ret < 0 || (unsigned)ret != sizeof current_page) {
-        APP_LOG(APP_LOG_LEVEL_ERROR, "unexpected return value %d for persist_wride_data", ret);
-        return;
-    }
-
-}
-
-static void new_event(AccelData data) {
-    
-	struct event event;
-    event.x = data.x;
-    event.y = data.y;
-    event.z = data.z;
-    event.did_vibrate = data.did_vibrate;
-    event.timestamp = data.timestamp;
-	append_event(&event);
-    
-}
+DataLoggingSessionRef accel_data_log;
 
 static void data_handler(AccelData *data, uint32_t num_samples) {
     
-    for (uint32_t i = 0; i < num_samples; i++) {
-        if (data[i].timestamp > 0) {
-            new_event(data[i]);
+    if (num_samples >= ACCEL_BATCH) {
+        
+        DataLoggingResult result = data_logging_log(accel_data_log, data, 1);
+    
+        if (result == DATA_LOGGING_BUSY) {
+            APP_LOG(APP_LOG_LEVEL_ERROR, "someone else is writing to this logging session");
+        } else if (result == DATA_LOGGING_FULL) {
+            APP_LOG(APP_LOG_LEVEL_ERROR, "no more space to save data");
+        } else if (result == DATA_LOGGING_NOT_FOUND) {
+            APP_LOG(APP_LOG_LEVEL_ERROR, "the logging session does not exist");
+        } else if (result == DATA_LOGGING_CLOSED) {
+            APP_LOG(APP_LOG_LEVEL_ERROR, "The logging session was made inactive");
+        } else if (result == DATA_LOGGING_INVALID_PARAMS) {
+            APP_LOG(APP_LOG_LEVEL_ERROR, "an invalid parameter was passed to one of the functions");
         }
+        
     }
     
 }
 
 static bool init(void) {
     
-	int ret = persist_read_data(1, current_page, sizeof current_page);
-	if (ret == E_DOES_NOT_EXIST) {
-		APP_LOG(APP_LOG_LEVEL_INFO, "no configuration found, initializing to zero");
-		memset(current_page, 0, sizeof current_page);
-	} else if (ret != sizeof current_page) {
-		APP_LOG(APP_LOG_LEVEL_ERROR, "unexpected return value %d for persist_read_data", ret);
-		return false;
-	} else if (current_page[0].timestamp) {
-		for (index = 1; index < PAGE_LENGTH && current_page[index - 1].timestamp < current_page[index].timestamp; index += 1);
-	} else {
-        index = 0;
-    }
-
-    accel_data_service_subscribe(num_samples, &data_handler);
+    accel_data_service_subscribe(ACCEL_BATCH, &data_handler);
     accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
-	return true;
+    
+    accel_data_log = data_logging_create(
+        0x1010,
+        DATA_LOGGING_BYTE_ARRAY,
+        sizeof(AccelData) * ACCEL_BATCH,
+        false
+    );
+    
+    return true;
     
 }
 
 static void deinit(void) {
     
+    data_logging_finish(accel_data_log);
     accel_data_service_unsubscribe();
     
 }
